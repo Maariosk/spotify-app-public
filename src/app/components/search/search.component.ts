7aa2268ba8e4c6ff263f578154093589719b1a1a
Mario C <maariosk@gmail.com>
2020-10-13 19:30:04 -0500

    search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  artistas: any [] = [];
 
-  constructor() { }
+  constructor(private spotify: SpotifyService) { }
+
+  buscar(termino: string){
+    this.spotify.getArtista(termino).subscribe( (data: any) => {
+      console.log(data);
+
+      this.artistas = data.artists.items;
+
+    });
+
+  }
 
   ngOnInit(): void {
   }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -20,5 +20,16 @@ export class SpotifyService {
 
      return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers});
 
+
+   }
+
+   getArtista(termino: string){
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQA0tBBcnMspSGj2oS-_sx_HAPlKH40yBZhHwpCQgFCvj7A7ejDnV8X4n6ASqNhebbwpDfbLcqsWkDJdgYE'
+
+    });
+
+     return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers} );
    }
 }
